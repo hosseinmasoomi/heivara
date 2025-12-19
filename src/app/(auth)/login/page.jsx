eeb3f938791fe356667563754052c0ed880f9121
hosseinmasoomi <Hh7575767@@>
2025-12-19 19:03:16 +0330

    fix runtime login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,4 +1,7 @@
-import LoginView from "../../../views/auth/LoginView";
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
+import LoginView from "@/views/auth/LoginView";
 
 export default function Page() {
   return <LoginView />;
